2f59ccfdaab588016229e0d0db90bb3e7aef9ad6
Wesley Heung <wesley222@gmail.com>
2015-05-17 16:48:12 -0400

    Some cleanup and documentation.

--- src/logic/QuadtreeNode.java
+++ src/logic/QuadtreeNode.java
@@ -10,15 +10,23 @@ import java.util.Set;
 
 ///http://gamedev.stackexchange.com/questions/31021/quadtree-store-only-points-or-regions
 public class QuadtreeNode implements Iterable<Shape> {
-	int MAX_OBJECTS = 5;
+	private int MAX_OBJECTS = 5;
 	private int MAX_DEPTH = 5;
 
+	/** The number of levels above this node to the root. */
 	private final int depth;
 	private List<Shape> objects;
 	public Rectangle bounds;
 	private QuadtreeNode parent;
 	public List<QuadtreeNode> nodes;
 
+	/**
+	 * Create a root QuadtreeNode. It has no parent by definition.
+	 * 
+	 * @param pBounds
+	 *            The bounding box of this level of quadtree. Any objects
+	 *            outside this area will be ignored.
+	 */
 	public QuadtreeNode(Rectangle pBounds) {
 		this(null, pBounds, 0);
 	}
@@ -26,17 +34,9 @@ public class QuadtreeNode implements Iterable<Shape> {
 	private QuadtreeNode(QuadtreeNode pparent, Rectangle pBounds, int idepth) {
 		parent = pparent;
 		objects = new ArrayList<>();
-		nodes = new ArrayList<>();
+		nodes = new ArrayList<>(4);
 		bounds = pBounds;
 		depth = idepth;
-	}
-
-	public void clear() {
-		objects.clear();
-		for (int i = 0; i < nodes.size(); i++) {
-			nodes.get(i).clear();
-		}
-		nodes.clear();
 	}
 
 	private void split() {
@@ -61,30 +61,44 @@ public class QuadtreeNode implements Iterable<Shape> {
 		objects.clear();
 	}
 
+	/**
+	 * Insert a new object into the quadtree.
+	 * 
+	 * @param s
+	 *            The bounding shape of the object
+	 * @return <b>true</b> if the insertion was successful.<br>
+	 *         <b>false</b> if the insertion failed because the object does not
+	 *         belong in this level of the quad tree.
+	 */
 	public boolean insert(Shape s) {
 		// Does the shape fit into this quadtree?
 		if (!s.getBounds().intersects(bounds)) {
 			return false;
 		}
 
-		if (nodes.isEmpty()) {
-			// Add to current node (leaf)
-			objects.add(s);
-			// Split
-			if (objects.size() > MAX_OBJECTS && depth < MAX_DEPTH) {
-				split();
-			}
+		// No split, add to existing children
+		if (!nodes.isEmpty()) {
 
+			for (QuadtreeNode st : nodes) {
+				st.insert(s);
+			}
 			return true;
 		}
 
-		// No split, add to existing children
-		for (QuadtreeNode st : nodes) {
-			st.insert(s);
+		// Add to current node (leaf)
+		objects.add(s);
+		// Split
+		if (objects.size() > MAX_OBJECTS && depth < MAX_DEPTH) {
+			split();
 		}
+
 		return true;
 	}
 
+	/**
+	 * @return A set containing all the objects at this level or below in the
+	 *         quadtree.
+	 */
 	public Set<Shape> getObjects() {
 		Set<Shape> retobj = new HashSet<>(objects);
 		for (QuadtreeNode st : nodes) {
@@ -95,16 +109,26 @@ public class QuadtreeNode implements Iterable<Shape> {
 
 	@Override
 	public Iterator<Shape> iterator() {
-		// Only retrieve unique objects from the tree
-		Set<Shape> allObjects = new HashSet<Shape>();
-		allObjects.addAll(getObjects());
-		return allObjects.iterator();
+		return getObjects().iterator();
 	}
 
+	/**
+	 * Gets the depth of this node in the quadtree structure. The depth is
+	 * defined as the number of levels of nodes above this node to the root.<br>
+	 * This function returns a stored value, so there are no performance
+	 * implications.
+	 * 
+	 * @return the depth of this node
+	 */
 	public int getDepth() {
 		return depth;
 	}
 
+	/**
+	 * Gets the greatest number of levels in the quadtree below this node.
+	 * 
+	 * @return the number of levels to the deepest part of the tree
+	 */
 	public int getDepthBelow() {
 		int ret = 0;
 		for (QuadtreeNode st : nodes) {
@@ -112,56 +136,4 @@ public class QuadtreeNode implements Iterable<Shape> {
 		}
 		return ret;
 	}
-
-	// private class Iter implements Iterator<Shape> {
-	// private Deque<QuadtreeNode> stack;
-	// private Deque<Shape> currentObjects;
-	// private Shape lastReturned;
-	//
-	// public Iter() {
-	// stack = new ArrayDeque<>();
-	// stack.push(QuadtreeNode.this);
-	//
-	// currentObjects = new ArrayDeque<Shape>();
-	// }
-	//
-	// @Override
-	// public boolean hasNext() {
-	// return !stack.isEmpty() || !currentObjects.isEmpty();
-	// }
-	//
-	// @Override
-	// public Shape next() {
-	// // Populate the current objects array
-	// while (currentObjects.isEmpty()) {
-	// // Find a leaf node
-	// QuadtreeNode top = stack.poll();
-	// if (top == null) {
-	// throw new NoSuchElementException();
-	// }
-	//
-	// while (top.nodes != null) {
-	// // Pop off the top and add its children to the stack
-	// for (QuadtreeNode st : top.nodes) {
-	// stack.push(st);
-	// }
-	// }
-	//
-	// currentObjects.addAll(top.objects);
-	// }
-	// // Actually return something
-	// lastReturned = currentObjects.pop();
-	// return lastReturned;
-	// }
-	//
-	// @Override
-	// public void remove() {
-	// if (lastReturned == null) {
-	// throw new IllegalStateException();
-	// }
-	// QuadtreeNode.this.remove(lastReturned);
-	// lastReturned = null;
-	// }
-	//
-	// }
 }
